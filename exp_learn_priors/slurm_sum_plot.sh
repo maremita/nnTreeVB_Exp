9e7024cd3f4d16295688d69913415071e47074f4
Amine M. Remita <remita.amine@courrier.uqam.ca>
2023-01-25 11:46:27 -0500

    update exp_learm_priors config files

--- exp_learn_priors/slurm_sum_plot.sh
+++ exp_learn_priors/slurm_sum_plot.sh
@@ -2,14 +2,22 @@
 
 #SBATCH --account=ctb-banire
 #SBATCH --mail-user=amine.m.remita@gmail.com
+
 ##SBATCH --job-name=01_exp_sum_plot
 ##SBATCH --job-name=02_2exp_sum_plot
 ##SBATCH --job-name=03_rates_sum_plot
 ##SBATCH --job-name=04_freqs_sum_plot
 #SBATCH --job-name=05_fit_sum_plot
+
 #SBATCH --cpus-per-task=24
-#SBATCH --mem=80000M
+
+##SBATCH --mem=32000M
+##SBATCH --mem=64000M
+#SBATCH --mem=240000M
+##SBATCH --mem=450000M
+
 #SBATCH --time=01:00:00
+
 #SBATCH --error=../results/exp_learn_priors/exp_jobs/tmp/%j.err
 #SBATCH --output=../results/exp_learn_priors/exp_jobs/tmp/%j.out
 
@@ -45,11 +53,19 @@ pip install --no-index $SLURM_TMPDIR/nnTreeVB/
 # MFG, data grid, different kappa values for rate estimation
 # 64G/1 hour
 #nntreevb_sum_plot_exps.py -c 03_mfg_gtr_rates_dhky.ini -j 03_mfg_gtr_rates_dhky_1
+#nntreevb_sum_plot_exps.py -c 03_mfg_gtr_rates_dhky.ini -j 03_mfg_gtr_rates_dhky_all_lpd
+#nntreevb_sum_plot_exps.py -c 03_mfg_gtr_rates_dhky_l1k_t16.ini -j 03_mfg_gtr_rates_dhky_l1k_t16
 
 # MFG, data grid, different frequencies values for estimation
 # 64G/1 hour
 #nntreevb_sum_plot_exps.py -c 04_mfg_gtr_freqs_dhky.ini -j 04_mfg_gtr_freqs_dhky_1
+#nntreevb_sum_plot_exps.py -c 04_mfg_gtr_freqs_dhky.ini -j 04_mfg_gtr_freqs_dhky_all_lpd
+##nntreevb_sum_plot_exps.py -c 04_mfg_gtr_freqs_dhky_l1k_t16.ini -j 04_mfg_gtr_freqs_dhky_l1k_t16
 
 # MFG, JC69 data, exponentionally simulated branch lengths, keep fit iteration results
 # 80G/1 hour
-nntreevb_sum_plot_exps.py -c 05_mfg_exp_jc69_keep_fit.ini -j 05_mfg_exp_jc69_keep_fit_1
+#nntreevb_sum_plot_exps.py -c 05_mfg_exp_jc69_keep_fit.ini -j 05_mfg_exp_jc69_keep_fit_1
+# 240G/1h
+nntreevb_sum_plot_exps.py -c 05_mfg_exp_jc69_keep_fit.ini -j 05_mfg_exp_jc69_keep_fit_l1k_t64
+##nntreevb_sum_plot_exps.py -c 05_mfg_exp_jc69_keep_fit_l1k_t16.ini -j 05_mfg_exp_jc69_keep_fit_l1k_t16
+
--- results/exp_learn_priors/exp_outputs/tar_gz.sh
+++ results/exp_learn_priors/exp_outputs/tar_gz.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=targz
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=32000M
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --error=../exp_jobs/tmp/%j.err
 #SBATCH --output=../exp_jobs/tmp/%j.out
 
@@ -13,4 +13,12 @@
 #tar -zcf 02_mfg_2exp_blens_jc69_1.tar.gz 02_mfg_2exp_blens_jc69_1/
 #tar -zcf 03_mfg_gtr_rates_dhky_l1k_t32_1.tar.gz 03_mfg_gtr_rates_dhky_l1k_t32_1/
 #tar -zcf 04_mfg_gtr_freqs_dhky_l1k_t32_1.tar.gz 04_mfg_gtr_freqs_dhky_l1k_t32_1/
-tar -zcf 05_mfg_exp_jc69_keep_fit_1.tar.gz 05_mfg_exp_jc69_keep_fit_1/
+#tar -zcf 05_mfg_exp_jc69_keep_fit_1.tar.gz 05_mfg_exp_jc69_keep_fit_1/
+#tar -zcf 05_mfg_exp_jc69_keep_fit_l1k_t64.tar.gz 05_mfg_exp_jc69_keep_fit_l1k_t64/
+
+tar -zcf 03_mfg_gtr_rates_dhky_all_lpd.tar.gz 03_mfg_gtr_rates_dhky_all_lpd/
+tar -zcf 04_mfg_gtr_freqs_dhky_all_lpd.tar.gz 04_mfg_gtr_freqs_dhky_all_lpd/
+
+tar -zcf 03_mfg_gtr_rates_dhky_l1k_t16.tar.gz 03_mfg_gtr_rates_dhky_l1k_t16/
+tar -zcf 04_mfg_gtr_freqs_dhky_l1k_t16.tar.gz 04_mfg_gtr_freqs_dhky_l1k_t16/
+tar -zcf 05_mfg_exp_jc69_keep_fit_l1k_t16.tar.gz 05_mfg_exp_jc69_keep_fit_l1k_t16/
