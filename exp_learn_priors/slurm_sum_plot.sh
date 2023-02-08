01ebcc5c1b029c9824a4b33b37961fe7b9cca41a
Amine M. Remita <remita.amine@courrier.uqam.ca>
2023-02-08 11:38:53 -0500

    Preprint exp_learn_priors final config experiments

--- exp_learn_priors/slurm_sum_plot.sh
+++ exp_learn_priors/slurm_sum_plot.sh
@@ -13,10 +13,11 @@
 
 ##SBATCH --mem=32000M
 ##SBATCH --mem=64000M
+##SBATCH --mem=80000M
 #SBATCH --mem=240000M
 ##SBATCH --mem=450000M
 
-#SBATCH --time=01:00:00
+#SBATCH --time=01:30:00
 
 #SBATCH --error=../results/exp_learn_priors/exp_jobs/tmp/%j.err
 #SBATCH --output=../results/exp_learn_priors/exp_jobs/tmp/%j.out
@@ -52,20 +53,12 @@ pip install --no-index $SLURM_TMPDIR/nnTreeVB/
 
 # MFG, data grid, different kappa values for rate estimation
 # 64G/1 hour
-#nntreevb_sum_plot_exps.py -c 03_mfg_gtr_rates_dhky.ini -j 03_mfg_gtr_rates_dhky_1
 #nntreevb_sum_plot_exps.py -c 03_mfg_gtr_rates_dhky.ini -j 03_mfg_gtr_rates_dhky_all_lpd
-#nntreevb_sum_plot_exps.py -c 03_mfg_gtr_rates_dhky_l1k_t16.ini -j 03_mfg_gtr_rates_dhky_l1k_t16
 
 # MFG, data grid, different frequencies values for estimation
 # 64G/1 hour
-#nntreevb_sum_plot_exps.py -c 04_mfg_gtr_freqs_dhky.ini -j 04_mfg_gtr_freqs_dhky_1
 #nntreevb_sum_plot_exps.py -c 04_mfg_gtr_freqs_dhky.ini -j 04_mfg_gtr_freqs_dhky_all_lpd
-##nntreevb_sum_plot_exps.py -c 04_mfg_gtr_freqs_dhky_l1k_t16.ini -j 04_mfg_gtr_freqs_dhky_l1k_t16
 
 # MFG, JC69 data, exponentionally simulated branch lengths, keep fit iteration results
-# 80G/1 hour
-#nntreevb_sum_plot_exps.py -c 05_mfg_exp_jc69_keep_fit.ini -j 05_mfg_exp_jc69_keep_fit_1
 # 240G/1h
 nntreevb_sum_plot_exps.py -c 05_mfg_exp_jc69_keep_fit.ini -j 05_mfg_exp_jc69_keep_fit_l1k_t64
-##nntreevb_sum_plot_exps.py -c 05_mfg_exp_jc69_keep_fit_l1k_t16.ini -j 05_mfg_exp_jc69_keep_fit_l1k_t16
-
